73f904a274d85a85224f178084300d71aa8e5f69
Janek1010 <janekdomozych99@gmail.com>
2024-02-13 09:47:44 +0100

    FEAT: proper DTOs for response

--- src/main/java/org/example/githubservice/model/RepositoryDTO.java
+++ src/main/java/org/example/githubservice/model/RepositoryDTO.java
@@ -1,4 +1,13 @@
 package org.example.githubservice.model;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class RepositoryDTO {
+    private String fork;
+    private String name;
+    private OwnerDTO owner;
+
 }
